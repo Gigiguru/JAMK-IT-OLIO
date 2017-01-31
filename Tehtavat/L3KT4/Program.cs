e81e50ab57966bcaf949707e1f2d124f442312ab
Kaizuka <Kaizuka@DESKTOP-VB09GT1>
2017-01-31 13:04:32 +0200

    fixausta

---
+++ Tehtavat/L3KT4/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testi
+{
+
+    class Vehicle
+    {
+
+        public string Name { get; }
+
+        int speed;
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (!(value >= 0 && value <= 200))
+                    speed = value;
+                else
+                    speed = 0;
+            }
+        }
+
+        int tyres;
+        public int Tyres
+        {
+            get
+            {
+                return tyres;
+            }
+            set
+            {
+                if (!(value >= 10 && value <= 25))
+                    tyres = 14;
+                else
+                    tyres = value;
+            }
+        }
+
+        public Vehicle(string name, int vechileSpeed, int tires)
+        {
+            if (!(speed >= 0 && speed <= 200))
+                vechileSpeed = 0;
+
+            if (tyres >= 10 && tyres <= 25)
+                tires = 14;
+
+            speed = vechileSpeed;
+            tyres = tires;
+            Name = name;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Vechile: " + Name);
+            Console.WriteLine("Speed: " + Speed);
+            Console.WriteLine("Tyres: " + Tyres);
+        }
+
+        public override string ToString()
+        {
+            string s = Name + ", " + Speed + ", " + Tyres;
+            return "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle auto0 = new Vehicle ("Opel", 80, 18);
+            Vehicle auto1 = new Vehicle("Fiat", 150, 14);
+            Vehicle auto2 = new Vehicle("Lada", 199, 15);
+            Vehicle auto3 = new Vehicle("auto", 200, 25);
+
+            auto0.PrintData();
+            auto1.PrintData();
+            auto2.PrintData();
+            auto3.PrintData();
+
+            auto0.ToString();
+            auto1.ToString();
+            auto3.ToString();
+            auto3.ToString();
+        }
+
+    }
+}
---
+++ Tehtavat/L3KT5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT5_Opiskelija_
+{
+    class Opiskelija
+    {
+        string tunnus;
+        public string Tunnus
+        { get; set; }
+
+        public string Enimi
+        { get; set; }
+
+        public string Snimi
+        { get; set; }
+        public override string ToString()
+        {
+            string s = tunnus + ", " + Enimi + ", " + Snimi;
+            return s;
+        }
+        public void Tayta()
+        {
+            Console.WriteLine("Syötä Tunnus");
+            tunnus = Console.ReadLine();
+            Console.WriteLine("Syötä Etunimi");
+            Enimi = Console.ReadLine();
+            Console.WriteLine("Syötä Sukunimi");
+            Snimi = Console.ReadLine();
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Opiskelija[] oppi = new Opiskelija[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    oppi[i] = new Opiskelija();
+                    oppi[i].Tayta();
+
+                }
+                foreach (Opiskelija o in oppi)
+                {
+                    Console.WriteLine(o.ToString());
+                }
+            }
+        }
+    }
+}
