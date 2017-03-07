8b2137eb42cb3b5fae41c4f43e39988c2ecc1ed2
K3237 <K3237@ICT-421-WS-38>
2017-03-07 17:08:12 +0200

    .

---
+++ Tehtavat/DemoL08/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoL08
+{
+    interface ICalculator
+    {
+        int Add(int numero1, int numero2);
+        int Multiply(int numero1, int numero2);
+        int Subract(int numero1, int numero2);
+        int Divide(int numero1, int numero2);
+    }
+    //luokka joka toteuttaa Icalculator-rajapinnan
+    public class Calculator : ICalculator
+    {
+        public int Add(int numero1, int numero2)
+        { return numero1 + numero2; }
+        public int Multiply(int numero1, int numero2)
+        { return numero1 * numero2; }
+        public int Subract(int numero1, int numero2)
+        { return numero1 - numero2; }
+        public int Divide(int numero1, int numero2)
+        { return numero1 / numero2; }
+    }
+    class TestiPeti
+    {
+        
+        static void Main(string[] args)
+        {
+            //kokeillaan luokkaa calculator
+            Calculator laskin = new Calculator();
+            int tulos = laskin.Add(10, 20);
+            Console.WriteLine("Yhteenlasku 10+20 tulos on" + tulos);
+        }
+    }
+}
---
+++ Tehtavat/L08T1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L08T1
+{
+    interface ICLASS
+    {
+        int Heita(int Numero1, int heitto);
+    }
+    public int Numero
+    {
+        get
+        {
+            return Numero;
+        }
+        set
+        {
+            if (!(value >= 0 && value <= 200))
+                numero = value;
+            else
+                numero = 0;
+        }
+    }
+    public class Dice : ICLASS
+    {
+        public int Heita(int Numero1, int heitto)
+        { 
+        return Numero1 + heitto;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kokeillaan luokkaa calculator
+            Dice laskin = new Dice();
+            int tulos = laskin.Heita(10, 20);
+            Console.WriteLine("Yhteenlasku 10+20 tulos on" + tulos);
+        }
+    }
+}
