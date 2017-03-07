619af46e2e081db32a0ff692a1d7181364affbdb
Kaizuka <Kaizuka@DESKTOP-VB09GT1>
2017-03-07 11:36:09 +0200

    L7T1 ja T2 tehty

---
+++ Tehtavat/L7T1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> List = new List<string>();
+            while (true)
+            {
+                string Arrays;
+                Console.Write("Give a text line (enter ends)");
+                Arrays = Console.ReadLine();
+                List.Add(Arrays);
+                if (Arrays == "")
+                {
+                    goto Step1;
+                }
+            }
+
+
+        Step1:
+            using (StreamWriter sw = new StreamWriter("../../T1.txt"))
+            {
+                foreach (string item in List)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+            using (StreamReader sw = new StreamReader("../../T1.txt"))
+            {
+                string line;
+                while ((line = sw.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+        }
+    }
+}
---
+++ Tehtavat/L7T2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            using (StreamReader sw = new StreamReader("../../T1.txt"))
+            {
+                Person temp;
+                string line;
+                string[] split;
+
+                while ((line = sw.ReadLine()) != null)
+                {
+                    temp = new Person();
+                    split = line.Split(' ');
+                    temp.Firstname = split[0];
+
+                    persons.Add(temp);
+
+                }
+                Dictionary<string, int> Names = new Dictionary<string, int>();
+                for (int i = 0; i < persons.Count; i++)
+                {
+                    if (Names.ContainsKey(persons[i].Firstname))
+                    {
+                        Names[persons[i].Firstname]++;
+                    }
+                    else
+                    {
+                        Names.Add(persons[i].Firstname, 1);
+                    }
+                }
+                foreach (KeyValuePair<string, int> key in Names)
+                {
+                    Console.WriteLine(key.Key + " " + key.Value);
+                }
+
+            }
+        }
+    }
+}
