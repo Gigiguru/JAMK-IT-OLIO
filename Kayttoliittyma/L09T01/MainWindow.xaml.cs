ea6aae262ddf140845deed3ac8b11c16f9d7ef5e
Kaizuka <Kaizuka@DESKTOP-VB09GT1>
2017-03-28 12:58:41 +0300

    testi

---
+++ Kayttoliittyma/L09T01/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L09T1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int laskuriC = 0;
+        private int laskuriT = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCars_Click(object sender, RoutedEventArgs e)
+        {
+
+            laskuriC++;
+            txtCars.Text = laskuriC.ToString();
+        }
+
+        private void btnTrucks_Click(object sender, RoutedEventArgs e)
+        {
+            laskuriT++;
+            txtTrucks.Text = laskuriT.ToString();
+
+        }
+    }
+}
