a3dd7f32cf47d15ae1aa32fcce428a8f594f0061
Kaizuka <Kaizuka@DESKTOP-VB09GT1>
2017-03-28 13:01:55 +0300

    L09 korjaus

---
+++ Kayttoliittyma/L09T02/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L09T2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtInput.Text, "[^0-9,]"))
+            {
+                MessageBox.Show("Syota luku");
+                txtInput.Text = txtInput.Text.Remove(txtInput.Text.Length - 1);
+            }
+        }
+
+        private void chMarkka_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)chMarkka.IsChecked)
+            {
+                chEuro.IsChecked = false;
+            }
+        }
+
+        private void chEuro_Checked(object sender, RoutedEventArgs e)
+        {
+            if ((bool)chEuro.IsChecked)
+            {
+                chMarkka.IsChecked = false;
+            }
+        }
+
+        private void Laske_Click(object sender, RoutedEventArgs e)
+        {
+            double Euro = 5.945;
+            double Markka = 0.168;
+            double Value = 0;
+            if (chMarkka.IsChecked == false && chEuro.IsChecked == false)
+            {
+                MessageBox.Show("valitse vaihtoehto");
+            }
+            else if ((bool)chMarkka.IsChecked)
+            {
+                double temp = 0;
+                double.TryParse(txtInput.Text, out Value);
+                temp = Value * Markka;
+                txtOutput.Text = temp.ToString("0.00") + " euroa ";
+            }
+            else if ((bool)chEuro.IsChecked)
+            {
+                double temp = 0;
+                double.TryParse(txtInput.Text, out Value);
+                temp = Value * Euro;
+                txtOutput.Text = temp.ToString("0.00") + " Markkaa ";
+            }
+
+        }
+    }
+}
---
+++ Kayttoliittyma/L09T03/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtKarmi.Text, "[^0-9,]"))
+            {
+                MessageBox.Show("Syota luku");
+                txtKarmi.Text = txtKarmi.Text.Remove(txtKarmi.Text.Length - 1);
+            }
+
+        }
+
+        private void txtH_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtH.Text, "[^0-9,]"))
+            {
+                MessageBox.Show("Syota luku");
+                txtH.Text = txtH.Text.Remove(txtH.Text.Length - 1);
+
+            }
+        }
+
+        private void txtL_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtL.Text, "[^0-9,]"))
+            {
+                MessageBox.Show("Syota luku");
+                txtL.Text = txtL.Text.Remove(txtL.Text.Length - 1);
+            }
+        }
+        private double Ikkunanpintaala()
+        {
+
+            double L, H = 0;
+            double.TryParse(txtL.Text, out L);
+            double.TryParse(txtH.Text, out H);
+            return L * H;
+        }
+        private double Lasinpintaala()
+        {
+
+            double L, H, K = 0;
+            double.TryParse(txtL.Text, out L);
+            double.TryParse(txtH.Text, out H);
+            double.TryParse(txtKarmi.Text, out K);
+            return (L - (2 * K)) * (H - (2 * K));
+        }
+        private double Karminpiiri()
+        {
+
+            double L, H, Value = 0;
+            double.TryParse(txtL.Text, out L);
+            double.TryParse(txtH.Text, out H);
+            Value = (((L * 2) + (H * 2)) / 10);
+            return Value;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            txtBIA.Text = Ikkunanpintaala().ToString() + "CM^2";
+            txtBLA.Text = Lasinpintaala().ToString() + "CM^2";
+            txtKP.Text = Karminpiiri().ToString() + " CM";
+        }
+    }
+}
